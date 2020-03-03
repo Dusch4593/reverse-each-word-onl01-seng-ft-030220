604d17acdeb5915e0768240826ca992aa17ec644
Brandon Dusch <brandondusch@gmail.com>
2020-03-03 20:59:01 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ require 'pry'
 def reverse_each_word(sentence)
   sentence_array = sentence.split(" ")
   new_sentence = []
-  sentence_array.each{|word|
+  sentence_array.collect{|word|
     new_sentence << word.reverse 
   }
   new_sentence.join(" ")
